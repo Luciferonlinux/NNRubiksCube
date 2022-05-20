3b89da024d9d28f3b5823b309ec1086ef99d93f9
Luciferonlinux <50619620+Luciferonlinux@users.noreply.github.com>
2022-05-20 15:52:27 +0200

    20.5. Before tensorflow install attempt

--- Pattern Recognition.py
+++
@@ -1,1 +0,0 @@
-# use pycuber method for cfop
--- Rubiks Cube.py
+++ Rubiks Cube.py
@@ -1,8 +1,7 @@
 import pycuber as pc
 from ScrambleGenerator import FullScramble
 
-# Todo: Cube in form of a matrix for pattern recognition
-# Todo: Make Pattern Recognition for backpropagation (white cross)
+# todo create dataset(s) for the ai to train on (0-20 moves)
 
 
 # create a fully scrambled Cube
