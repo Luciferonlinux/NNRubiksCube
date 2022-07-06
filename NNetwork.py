c1664efed0487a1e6d82101817a2a94b69b114fe
Luciferonlinux <50619620+Luciferonlinux@users.noreply.github.com>
2022-07-06 18:14:49 +0200

    List representation works

--- NNetwork.py
+++ NNetwork.py
@@ -1,5 +1,31 @@
 import tensorflow as tf
-import numpy as np
-import os
-import time
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Embedding, Conv3D, Input
 
+
+def convtotensor(arg):
+    """
+    Takes any list or tuple or nparray and converts it to a tf.Tensor
+    """
+    arg = tf.convert_to_tensor(arg, dtype=tf.float32)
+    return arg
+
+
+def NN(cubelist):
+    """
+    Takes the Cube as List and runs it through the Neural network
+    """
+
+    cubetensor = convtotensor(cubelist)
+    shape = cubetensor.shape
+    return shape
+    # model = Sequential()
+    # model.add(
+    #    Embedding(
+    #        input_dim=cubetensor.tf.tensor
+    #
+    #    )
+    # )
+
+
+NN(convtotensor([1, 1, 1, 2, 2, 3, 5, 1, 2, 4, 2, 3, 4, 1]))
